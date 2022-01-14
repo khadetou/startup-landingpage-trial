49c27a7fb9e82cc0a3bf55ea3fffad9c69c3aaf3
khadetou <khadetou96@gmail.com>
2022-01-14 18:38:57 +0000

    styling fixing

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -9,7 +9,7 @@ interface HeaderProps {
 
 const Header: FC<HeaderProps> = ({ className }) => {
     return (
-        <header className={`text-primary font-body font-normal py-5 w-full absolute top-0 left-0 bg-transparent transition-all ease-in duration-[0.4s] animate-positionAnim ${className}`}>
+        <header className={`text-primary font-body font-normal py-5 w-full absolute min-w-0 top-0 left-0 bg-transparent transition-all ease-in duration-[0.4s] animate-positionAnim ${className}`}>
             <div className="flex justify-between items-center max-w-full w-full px-5 xs:px-[30px] md:max-w-[780px] lg:max-w-[1020px] reg:max-w-[1200px] 3xl:max-w-[1310px] mx-auto">
                 <Logo src={LogoImage} />
                 <nav className="mx-auto hidden lg:block nav ">
