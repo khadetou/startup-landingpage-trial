b96450b68b39fa7fdb892056d8b6fbfe1e24dc34
khadetou <khadetou96@gmail.com>
2022-01-14 13:33:52 +0000

    nav data

---
+++ src/components/header/data.ts
@@ -0,0 +1,18 @@
+export const navigationLinks = [
+  {
+    path: "home",
+    label: "Home",
+  },
+  {
+    path: "features",
+    label: "Features",
+  },
+  {
+    path: "pricing",
+    label: "Pricing",
+  },
+  {
+    path: "testimonials",
+    label: "Testimonials",
+  },
+];
---
+++ src/components/header/mobile-drawer.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react"
+
+const MobileDrawer: FC = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default MobileDrawer
