2feafb3408c1f5543df0e44587137c35b1a8cbdc
khadetou <khadetou96@gmail.com>
2022-01-18 14:05:54 +0000

    feature section

--- src/components/feature-card-column.tsx
+++ src/components/feature-card-column.tsx
@@ -13,7 +13,7 @@ const FeatureCardColumn: FC<FeatureCardColumnProps> = ({ src, altText = "default
             className="flex items-center xs:items-start flex-col -mb-[5] text-center sm:text-left px-5 sm:px-0"
         >
             <div
-                className="relative mx-auto sm:mx-0 mt-auto sm:mt-[-13px] mb-[-10px] w-20 md:w-[90px] reg:w-auto">
+                className="relative mx-auto sm:mx-0 mt-auto sm:mt-[-13px] mb-[-10px] w-20 md:w-[90px] reg:w-auto h-28">
                 <Image layout="fill" src={src} alt={altText} />
             </div>
 
@@ -25,11 +25,11 @@ const FeatureCardColumn: FC<FeatureCardColumnProps> = ({ src, altText = "default
                 >
                     {title}
                 </h2>
-                <p
-                    className="text-[15px] font-normal leading-[1.9]"
+                <span
+                    className="text-[15px] font-normal text-primary leading-[1.9]"
                 >
                     {text}
-                </p>
+                </span>
             </div>
         </div>
     )
--- src/sections/key-features.tsx
+++ src/sections/key-features.tsx
@@ -1,19 +1,34 @@
+import FeatureCardColumn from "components/feature-card-column";
 import SectionHeader from "components/section-header";
 import { FC } from "react";
-
+import { keyfeatureDate } from "./data";
 const KeyFeatures: FC = () => {
     return (
         <section
             className="sm:py-[100px] reg:py-[120px] pt-[80px] pb-[90px]"
             id="features">
-            <SectionHeader
-                slogan="What's the function"
-                title="Meet the feature of our product"
-            />
             <div
-                className="xs:w-4/5 sm:w-full sm:gap-10 sm:grid-cols-2 md:gap-x-[60px] md:gap-y-[50px] lg:gap-[30px] lg:grid-cols-4 reg:gap-y-[50px] reg:gap-x-10 xl:gap-y-[55px] xl:gap-x-[90px] box-border m-0 min-w-0 grid gap-y-[35px] gap-x-0 w-ful mx-auto  grid-cols-1"
+                className="xs:px-[30px] md:max-w-[780px] lg:max-w-[1020px] reg:max-w-[1200px] box-border m-0 min-w-0 w-full mx-auto max-w-full px-5"
             >
-
+                <SectionHeader
+                    slogan="What's the function"
+                    title="Meet the feature of our product"
+                />
+                <div
+                    className="xs:w-4/5 sm:w-full sm:gap-10 sm:grid-cols-2 md:gap-x-[60px] md:gap-y-[50px] lg:gap-[30px] lg:grid-cols-4 reg:gap-y-[50px] reg:gap-x-10 xl:gap-y-[55px] xl:gap-x-[90px] box-border m-0 min-w-0 grid gap-y-[35px] gap-x-0 w-ful mx-auto  grid-cols-1"
+                >
+                    {
+                        keyfeatureDate.map((item) => (
+                            <FeatureCardColumn
+                                key={item.id}
+                                title={item.title}
+                                src={item.imgSrc}
+                                text={item.text}
+                                altText={item.altText}
+                            />
+                        ))
+                    }
+                </div>
             </div>
         </section>
     )
